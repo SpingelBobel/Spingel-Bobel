3e66c5104355e70e9737cbb0dbe8103051abee6e
dovedevic <dovedevic@gmail.com>
2021-04-28 03:07:05 -0400

    initial transfer of v4 conch using dep env

---
+++ deploy_enironment.py
@@ -0,0 +1,13 @@
+import asyncio
+import sys
+
+from src.utils import logging
+from src.utils import common
+from src import conch as conch_bot
+
+DEBUG_MODE = not (len(sys.argv) == 2 and sys.argv[1] == "prod")
+
+loop = asyncio.get_event_loop()
+conch = loop.create_task(conch_bot.conch_bot.start(common.load_creds(DEBUG_MODE, common.EcosystemBots.MagicConch)))
+gathered = asyncio.gather(conch, loop=loop)
+loop.run_until_complete(gathered)
