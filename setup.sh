5312858a404e098af79f361b3888218aee843ec8
SpingelBobel <150081214+SpingelBobel@users.noreply.github.com>
2023-11-06 13:29:21 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 # !bin/sh
-cd ~/Servers/magicconch/
-python3.8 ~/Servers/magicconch/deploy_environment.py prod > "./logs/$(date +'%Y-%m-%d_%Hh%Mm').console.log" 2>&1 &
+cd ~/Servers/spingelbobel/
+python3.8 ~/Servers/spingelbobel/deploy_environment.py prod > "./logs/$(date +'%Y-%m-%d_%Hh%Mm').console.log" 2>&1 &
 ps ax | grep environment.py
