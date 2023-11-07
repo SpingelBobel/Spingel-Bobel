89350df95a46faf60bb0a8d1a9bc9d16aab4838b
SpingelBobel <150081214+SpingelBobel@users.noreply.github.com>
2023-11-07 11:54:38 -0800

    Update checks.py

--- src/utils/checks.py
+++ src/utils/checks.py
@@ -2,10 +2,10 @@ from discord.ext import commands
 
 
 def _is_dove(user):
-    return user.id == 304695409031512064
-
+    return user.id == 1171203015496175677
 
 def admin():
     async def pred(ctx):
         return _is_dove(ctx.author)
+   
     return commands.check(pred)
